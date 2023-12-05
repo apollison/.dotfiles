742fa6091937ad757337e7e5eed14ae95c35726b
Andrew Pollison <36175307+apollison@users.noreply.github.com>
2023-12-04 17:41:06 -0700

    tweak harpoon hotkeys to suck less

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -12,10 +12,11 @@ harpoon:setup()
 -- vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
 -- vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
 
--- vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end)
--- vim.keymap.set("n", "<leader>hl", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
---
--- vim.keymap.set("n", "<leader>hh", function() harpoon:list():select(1) end)
--- vim.keymap.set("n", "<leader>hj", function() harpoon:list():select(2) end)
--- vim.keymap.set("n", "<leader>hk", function() harpoon:list():select(3) end)
--- vim.keymap.set("n", "<leader>hl", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<leader>u", function() harpoon:list():append() end)
+vim.keymap.set("n", "<leader>i", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+
