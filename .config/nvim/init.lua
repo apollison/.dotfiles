2817f7d0bfd12d8bc540da556681250b18ab6ba8
Andrew Pollison <36175307+apollison@users.noreply.github.com>
2023-12-10 14:05:10 -0700

    add working neotest configs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -243,6 +243,7 @@ require('lazy').setup({
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
   -- { import = 'custom.plugins' },
+  { import = 'apollison.plugins' },
 }, {})
 
 -- [[ Setting options ]]
--- .config/nvim/lua/apollison/keymaps.lua
+++ .config/nvim/lua/apollison/keymaps.lua
@@ -5,8 +5,8 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = 'Go to Netrw' })
 vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { desc = '[F]ormat' })
 
 -- neovim.test mappings.
--- vim.keymap.set('n', '<leader>tt', require("neotest").run.run, {desc = "Run [T]est"})
--- vim.keymap.set('n', '<leader>ts', require("neotest").summary.toggle, {desc = "Toggle [T]est [S]ummary"})
--- vim.keymap.set('n', '<leader>tn', require("neotest").jump.next, {desc = "Next Test"})
--- vim.keymap.set('n', '<leader>tp', require("neotest").jump.prev, {desc = "Prev Test"})
+vim.keymap.set('n', '<leader>tt', require("neotest").run.run, {desc = "Run [T]est"})
+vim.keymap.set('n', '<leader>ts', require("neotest").summary.toggle, {desc = "Toggle [T]est [S]ummary"})
+vim.keymap.set('n', '<leader>tn', require("neotest").jump.next, {desc = "Next Test"})
+vim.keymap.set('n', '<leader>tp', require("neotest").jump.prev, {desc = "Prev Test"})
 
