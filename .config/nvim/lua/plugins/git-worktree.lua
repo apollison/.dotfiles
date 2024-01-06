243c0a00f0e2ec414a863acea1fb3ac547773a9f
apollison <apollison@gmail.com>
2024-01-06 08:47:10 -0700

    git-worktree change message and remap home dir in tmux

--- .config/nvim/lua/plugins/git-worktree.lua
+++ .config/nvim/lua/plugins/git-worktree.lua
@@ -5,6 +5,12 @@ return {
     tele.load_extension("git_worktree")
     vim.keymap.set('n', '<leader>gws', tele.extensions.git_worktree.git_worktrees, { desc = 'git worktree search' })
     vim.keymap.set('n', '<leader>gwc', tele.extensions.git_worktree.create_git_worktree, { desc = 'git worktree create' })
+
+    local wt = require("git-worktree")
+    wt.on_tree_change(function(op, metadata)
+      if op == wt.Operations.Switch then
+        print("Switched from " .. metadata.prev_path .. " to " .. metadata.path)
+      end
+    end)
   end,
 }
-
