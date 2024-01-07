5beede49659d497578ab91a4bbe3891c0b602f2d
Andrew Pollison <36175307+apollison@users.noreply.github.com>
2024-01-07 11:29:30 -0700

    nvim: Add gruvebox color theme.
    TODO: Maybe load all color themes and be able to switch? Meh.

--- config/.config/nvim/lua/plugins/color-scheme.lua
+++ config/.config/nvim/lua/plugins/color-scheme.lua
@@ -10,11 +10,22 @@
 --   end,
 -- }
 
+-- return {
+--   'catppuccin/nvim',
+--   name = 'catppuccin',
+--   priority = 1000,
+--   config = function()
+--     vim.cmd.colorscheme 'catppuccin-mocha'
+--   end,
+-- }
+
 return {
-  'catppuccin/nvim',
-  name = 'catppuccin',
+  'ellisonleao/gruvbox.nvim',
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme 'catppuccin-mocha'
+    vim.o.background = "dark"
+    vim.cmd([[colorscheme gruvbox]])
   end,
+  opts = {},
 }
+
