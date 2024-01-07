6af1a0302a7750208bb5593b219c7bfa0ea24de6
Andrew Pollison <36175307+apollison@users.noreply.github.com>
2024-01-07 11:26:39 -0700

    nvim: Add some nice keymaps for window management and a quick quitter.

--- config/.config/nvim/lua/config/keymap.lua
+++ config/.config/nvim/lua/config/keymap.lua
@@ -12,3 +12,14 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
+-- Scrolling keymaps, keep the curser in the middle of the screen.
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+
+vim.keymap.set('n', '<leader>z', ':q<CR>')
+
+vim.keymap.set("n", "=", [[<cmd>vertical resize +5<cr>]]) -- make the window biger vertically
+vim.keymap.set("n", "-", [[<cmd>vertical resize -5<cr>]]) -- make the window smaller vertically
+vim.keymap.set("n", "+", [[<cmd>horizontal resize +2<cr>]]) -- make the window bigger horizontally by pressing shift and =
+vim.keymap.set("n", "_", [[<cmd>horizontal resize -2<cr>]]) -- make the window smaller horizontally by pressing shift and -
+
