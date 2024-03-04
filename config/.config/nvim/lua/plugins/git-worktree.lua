fa6bc24290a393c262bc81cd41dde17a6e5b927b
apollison <apollison@gmail.com>
2024-03-04 14:54:42 -0700

    nvim: Load local nvim config file when changing switching worktrees.

--- config/.config/nvim/lua/plugins/git-worktree.lua
+++ config/.config/nvim/lua/plugins/git-worktree.lua
@@ -10,6 +10,18 @@ return {
     wt.on_tree_change(function(op, metadata)
       if op == wt.Operations.Switch then
         print("Switched from " .. metadata.prev_path .. " to " .. metadata.path)
+
+        -- Change the cwd to the new worktree dir.
+        vim.api.nvim_set_current_dir(metadata.path)
+
+        -- Load any local config files for vim.
+        local config_file = metadata.path .. '/.nvim.lua'
+        local f = io.open(config_file, 'r')
+        if f then
+          dofile(metadata.path .. '/.nvim.lua')
+          print('Loaded .nvim.lua config file')
+          f:close()
+        end
       end
     end)
   end,
